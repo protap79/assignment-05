b5a9dd06651bd1747f736a894482c49fb9e51d8e
protap79 <protapsingh79@.com>
2025-03-01 13:01:34 +0600

    alert added

---
+++ fix-button.js
@@ -0,0 +1,25 @@
+// alert(`Board Updated Successfully`)
+
+document.getElementById("btn-fix").addEventListener("click", function () {
+    
+    alert(`Board Updated Successfully`);
+    // const decrease = document.getElementById("decrease").value;
+    // const convertedDecrease = parseInt("decrease")
+    //    let counterDiv = document.getElementById("decrease");
+    //    let currentNumber = parseInt(counterDiv.textContent);
+    //    counterDiv.textContent = currentNumber - 1;
+
+    //  let counterDiv = document.getElementById("increase");
+    //  let currentNumber = parseInt(counterDiv.textContent);
+    //  counterDiv.textContent = currentNumber + 1;
+
+    // const increase = document.getElementById("increase").value;
+    // const convertedIncrease = parseInt("increase")
+    // let increaseDiv = document.getElementById("increase");
+    // let decreaseDiv = document.getElementById("decrease");
+
+    // increaseDiv.innerText = parseInt(increaseDiv.innerText) + 1;
+    // decreaseDiv.innerText = parseInt(decreaseDiv.innerText) - 1;
+   
+    // document.getElementById("myButton").disabled = true;
+})
