f7fd3e2f3fde0c9a119e0edbe96c11c28c37b859
protap79 <protapsingh79@.com>
2025-03-02 20:37:52 +0600

    finally done with all

--- fix-button.js
+++ fix-button.js
@@ -1,6 +1,32 @@
-// alert(`Board Updated Successfully`)
+
 
 document.getElementById("btn-fix").addEventListener("click", function () {
+  alert(`Board Updated Successfully`);
+
+    const increase = document.getElementById("increase");
+    const decrease = document.getElementById("decrease");
+
+    increase.innerText = parseInt(increase.innerText) + 1;
+    decrease.innerText = parseInt(decrease.innerText) - 1;
+    
+  const btn = document.getElementById("btn-fix");
+  btn.style.opacity = "0.3";
+  btn.style.backgroundColor = "bg-indigo-600";
+    btn.style.pointerEvents = "none";
+    
+
+    if (increase.innerText > 28) {
+    alert(`Congrats! You have successfully completed`);
+}
+
+  const log = document.getElementById("log");
+  const time = new Date().toLocaleTimeString();
+  log.innerText = `You have completed the task Fix Mobile Button Issue at ${time}`;
+
+  log.style.display = "block";
+});
+
+document.getElementById("btn-cloud").addEventListener("click", function () {
   alert(`Board Updated Successfully`);
 
   const increase = document.getElementById("increase");
@@ -9,136 +35,108 @@ document.getElementById("btn-fix").addEventListener("click", function () {
   increase.innerText = parseInt(increase.innerText) + 1;
   decrease.innerText = parseInt(decrease.innerText) - 1;
 
-    
-    const btn = document.getElementById("btn-fix");
-    btn.style.opacity = "0.3";
-    btn.style.backgroundColor = "bg-indigo-600";
+  const btn = document.getElementById("btn-cloud");
+  btn.style.opacity = "0.3";
+  btn.style.backgroundColor = "bg-indigo-600";
     btn.style.pointerEvents = "none";
+    
+      if (increase.innerText > 28) {
+        alert(`Congrats! You have successfully completed`);
+      }
 
-  
-  const log = document.getElementById("log");
+  const log = document.getElementById("log-2");
   const time = new Date().toLocaleTimeString();
-    log.innerText = `You have completed the task Fix Mobile Button Issue at ${time}`;
-    
-     log.style.display = "block";
-
-
-
-})
-
-
-
-
-document.getElementById("btn-cloud").addEventListener("click", function () {
-    
-    alert(`Board Updated Successfully`);
-
-    const increase = document.getElementById("increase");
-    const decrease = document.getElementById("decrease");
-
-    increase.innerText = parseInt(increase.innerText) + 1;
-    decrease.innerText = parseInt(decrease.innerText) - 1;
-
-     const btn = document.getElementById("btn-cloud");
-     btn.style.opacity = "0.3";
-     btn.style.backgroundColor = "bg-indigo-600";
-     btn.style.pointerEvents = "none";
-
-
-    const log = document.getElementById("log-2");
-    const time = new Date().toLocaleTimeString();
-    log.innerText = `You have completed the task Add Pay Success Modal at ${time}`;
-
+  log.innerText = `You have completed the task Add Pay Success Modal at ${time}`;
 });
 
-
 document.getElementById("btn-swift").addEventListener("click", function () {
+  alert(`Board Updated Successfully`);
+
+  const increase = document.getElementById("increase");
+  const decrease = document.getElementById("decrease");
+
+  increase.innerText = parseInt(increase.innerText) + 1;
+  decrease.innerText = parseInt(decrease.innerText) - 1;
+
+  const btn = document.getElementById("btn-swift");
+  btn.style.opacity = "0.3";
+  btn.style.backgroundColor = "bg-indigo-600";
+    btn.style.pointerEvents = "none";
     
-    alert(`Board Updated Successfully`);
+      if (increase.innerText > 28) {
+        alert(`Congrats! You have successfully completed`);
+      }
 
-    const increase = document.getElementById("increase");
-    const decrease = document.getElementById("decrease");
-
-    increase.innerText = parseInt(increase.innerText) + 1;
-    decrease.innerText = parseInt(decrease.innerText) - 1;
-
-     const btn = document.getElementById("btn-swift");
-     btn.style.opacity = "0.3";
-     btn.style.backgroundColor = "bg-indigo-600";
-     btn.style.pointerEvents = "none";
-
-
-    const log = document.getElementById("log-3");
-    const time = new Date().toLocaleTimeString();
-    log.innerText = `You have completed the task Optimize Home Page at ${time}`;
-
-}); 
+  const log = document.getElementById("log-3");
+  const time = new Date().toLocaleTimeString();
+  log.innerText = `You have completed the task Optimize Home Page at ${time}`;
+});
 
 document.getElementById("btn-meta").addEventListener("click", function () {
+  alert(`Board Updated Successfully`);
+
+  const increase = document.getElementById("increase");
+  const decrease = document.getElementById("decrease");
+
+  increase.innerText = parseInt(increase.innerText) + 1;
+  decrease.innerText = parseInt(decrease.innerText) - 1;
+
+  const btn = document.getElementById("btn-meta");
+  btn.style.opacity = "0.3";
+  btn.style.backgroundColor = "bg-indigo-600";
+    btn.style.pointerEvents = "none";
     
-    alert(`Board Updated Successfully`);
+      if (increase.innerText > 28) {
+        alert(`Congrats! You have successfully completed`);
+      }
 
-    const increase = document.getElementById("increase");
-    const decrease = document.getElementById("decrease");
-
-    increase.innerText = parseInt(increase.innerText) + 1;
-    decrease.innerText = parseInt(decrease.innerText) - 1;
-
-     const btn = document.getElementById("btn-meta");
-     btn.style.opacity = "0.3";
-     btn.style.backgroundColor = "bg-indigo-600";
-     btn.style.pointerEvents = "none";
-
-
-    const log = document.getElementById("log-4");
-    const time = new Date().toLocaleTimeString();
-    log.innerText = `You have completed the task Add New Emoji at ${time}`;
-}); 
+  const log = document.getElementById("log-4");
+  const time = new Date().toLocaleTimeString();
+  log.innerText = `You have completed the task Add New Emoji at ${time}`;
+});
 
 document.getElementById("btn-google").addEventListener("click", function () {
-    
-    alert(`Board Updated Successfully`);
+  alert(`Board Updated Successfully`);
 
-    const increase = document.getElementById("increase");
-    const decrease = document.getElementById("decrease");
+  const increase = document.getElementById("increase");
+  const decrease = document.getElementById("decrease");
 
-    increase.innerText = parseInt(increase.innerText) + 1;
-    decrease.innerText = parseInt(decrease.innerText) - 1;
+  increase.innerText = parseInt(increase.innerText) + 1;
+  decrease.innerText = parseInt(decrease.innerText) - 1;
 
-    
-    const btn = document.getElementById("btn-google");
-    btn.style.opacity = "0.3";
-    btn.style.backgroundColor = "bg-indigo-600";
+  const btn = document.getElementById("btn-google");
+  btn.style.opacity = "0.3";
+  btn.style.backgroundColor = "bg-indigo-600";
     btn.style.pointerEvents = "none";
+    
+      if (increase.innerText > 28) {
+        alert(`Congrats! You have successfully completed`);
+      }
 
-
-    const log = document.getElementById("log-5");
-    const time = new Date().toLocaleTimeString();
-    log.innerText = `You have completed the task Integrate OpenAI API at ${time}`;
-
-}); 
+  const log = document.getElementById("log-5");
+  const time = new Date().toLocaleTimeString();
+  log.innerText = `You have completed the task Integrate OpenAI API at ${time}`;
+});
 
 document.getElementById("btn-glass").addEventListener("click", function () {
-    
-    alert(`Board Updated Successfully`);
+  alert(`Board Updated Successfully`);
 
-    const increase = document.getElementById("increase");
-    const decrease = document.getElementById("decrease");
+  const increase = document.getElementById("increase");
+  const decrease = document.getElementById("decrease");
 
-    increase.innerText = parseInt(increase.innerText) + 1;
-    decrease.innerText = parseInt(decrease.innerText) - 1;
+  increase.innerText = parseInt(increase.innerText) + 1;
+  decrease.innerText = parseInt(decrease.innerText) - 1;
 
-    const btn = document.getElementById("btn-glass");
-    btn.style.opacity = "0.3";
-    btn.style.backgroundColor = "bg-indigo-600";
+  const btn = document.getElementById("btn-glass");
+  btn.style.opacity = "0.3";
+  btn.style.backgroundColor = "bg-indigo-600";
     btn.style.pointerEvents = "none";
+    
+      if (increase.innerText > 28) {
+        alert(`Congrats! You have successfully completed`);
+      }
 
-
-
-    const log = document.getElementById("log-6");
-    const time = new Date().toLocaleTimeString();
-    log.innerText = `You have completed the task Improve Job Searching at ${time}`;
-
-}); 
-
-
+  const log = document.getElementById("log-6");
+  const time = new Date().toLocaleTimeString();
+  log.innerText = `You have completed the task Improve Job Searching at ${time}`;
+});
